85c8de5dafe00787eacbd777c637817c5b403db7
ryanscott2 <ryan_cycling@outlook.com>
2022-11-09 21:44:13 -0500

    amoungus

--- deleteRecord.py
+++ deleteRecord.py
@@ -10,18 +10,13 @@
 def delRecord():
     lastName = input('Enter the students last name: ')
     with open('file_records.txt', 'r+') as txt_file:
-        # read all lines in a list
         lines = txt_file.readlines()
-        txt_file.seek(0)
-        txt_file.truncate()
         for line in lines:
-            if line.find(lastName) != -1:
-                for number, line in enumerate(lines):
-                    # delete line number 5 and 8
-                    # note: list index start from 0
-                    if number not in []:
-                        txt_file.write(line)
+            if lastName not in line:
+                txt_file.write(line)
+    print(f'Record(s) with the last name {lastName} deleted.')
 
+delRecord()
 
 
 
